1f47bd07d0159fc2b88421bd0d66f189e3084ed3
Alejandra Aravena Silva - Everis <alejandra.aravena.silva@everis.com>
2018-03-27 13:01:42 -0300

    Actualizacion del fork, para gatillar nuevo heroko

--- server.js
+++ server.js
@@ -42,11 +42,11 @@ var eventCtrl = require('./controllers/event');
 var userCtrl = require('./controllers/user');
 
 // API routes
-
 app.use('/api', api);
 api.route('/author/:id').get(authorCtrl.getAuthor);
 api.route('/user').post(userCtrl.add);
 api.route('/authentication').post(userCtrl.authentication);
+//desde Event Controller
 api.route('/event').get(eventCtrl.getAllEvents);
 api.route('/event/:id').get(eventCtrl.getEventDetail);
 api.route('/comment/add').post(eventCtrl.addCommentToSpeech);
