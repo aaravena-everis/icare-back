ed86d266b22cd6384c101865daad315a5d537630
Alejandra Aravena Silva - Everis <alejandra.aravena.silva@everis.com>
2018-04-03 23:04:18 -0300

    Se agrega campo a version

--- models/version.js
+++ models/version.js
@@ -5,7 +5,8 @@ var versionSchema = new Schema({
 	nombre: { type : String, required: true},
 	desc: { type : String },
 	version: { type : String, required: true},
-	forzar: { type : Boolean, required: true}
+	forzar: { type : Boolean, required: true},
+	activar: { type: Boolean }
 });
 
 module.exports = mongoose.model('version', versionSchema);
