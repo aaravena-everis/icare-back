30aaf9a3bc24c3fe5f0c2251422b5a096494b029
Alejandra Aravena Silva - Everis <alejandra.aravena.silva@everis.com>
2018-05-16 22:24:55 -0400

    cambio bd a ICARE

--- config/config.js
+++ config/config.js
@@ -7,10 +7,13 @@ config.dbOptions = {
         poolSize: 10,
         reconnectTries: Number.MAX_VALUE
     },
-    user: 'everis-poc',
-    pass: 'everis-poc'
+    //user: 'everis-poc',
+    //pass: 'everis-poc'
+    user: 'icare_user',
+    pass: '1c4r3$$APP'
 };
-config.dbEndpoint = 'mongodb://ds153015.mlab.com:53015/heroku_gqscj5zg';
+//config.dbEndpoint = 'mongodb://ds153015.mlab.com:53015/heroku_gqscj5zg';
+config.dbEndpoint = 'mongodb://13.90.142.201/icare_mobile';
 config.web.port = process.env.PORT || 8080;
 config.secret = 'BkBUltR453Cr3TP4sSW0Rd';
 module.exports = config;
