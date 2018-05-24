c20aa2d034a4d3720f2990e69ea64f1128fa5f90
Alejandra Aravena Silva - Everis <alejandra.aravena.silva@everis.com>
2018-05-24 16:20:40 -0400

    nueva BD

--- config/config.js
+++ config/config.js
@@ -5,15 +5,14 @@ config.dbOptions = {
     db: { native_parser: true },
     server: {
         poolSize: 10,
-        reconnectTries: Number.MAX_VALUE
+        reconnectTries: Number.MAX_VALUE,
+        ssl: true
     },
-    //user: 'everis-poc',
-    //pass: 'everis-poc'
-    user: 'icare_user',
-    pass: '1c4r3$$APP'
+    user: 'icappmongo',
+    pass: '58Ozdus3NFfPf5pky1PF5LpINEsz2oBTHiPXq28QRRIzUIQf2h00VZfwK3Zwf17mwIIWWWOmonG6BSiCUUd2sQ=='
 };
-//config.dbEndpoint = 'mongodb://ds153015.mlab.com:53015/heroku_gqscj5zg';
-config.dbEndpoint = 'mongodb://13.90.142.201/icare_mobile';
+config.dbEndpoint = 'mongodb://icappmongo.documents.azure.com:10255/mobileapp?ssl=true&replicaSet=globaldb';
+
 config.web.port = process.env.PORT || 8080;
 config.secret = 'BkBUltR453Cr3TP4sSW0Rd';
 module.exports = config;
